a74dc056b9ac0a78133352a7af3f0a0a228bbf04
rajesh basnet <50390782+rajeshbasnet@users.noreply.github.com>
2023-07-06 17:36:26 +0545

    Added eureka discover server as a service registry

--- authService/src/test/java/com/treeleaf/server/AuthServiceApplicationTests.java
+++ authService/src/test/java/com/treeleaf/server/AuthServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.treeleaf.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AuthServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
